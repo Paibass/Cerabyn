e47e644ccceb8ce39db547aca9677897a9443560
paibass <joancho2012@hotmail.com.ar>
2022-08-29 15:47:07 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+    <Navbar/>
       <h1>¡Pagina en Construcción!</h1>
       <h2>CERABYN</h2>
       <p>Vuelva Pronto!</p>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+	return (
+		<div class="header">
+			<div class ="titulo">
+				<h1>Cerabyn</h1>
+			</div>
+			<div class ="navbar">
+				<ul>
+					<li>Inicio</li>
+					<li>Productos</li>
+					<li>Nosotros</li>
+				</ul>
+			</div>
+		</div>
+	)
+}
+
+export default Navbar
